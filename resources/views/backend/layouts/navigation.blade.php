65e66d9256e26a464c8b82f99442b2a61e92b3a4
agungsuhendra <agung.suhendra213@gmail.com>
2018-12-21 17:09:00 +0800

    perbaikan navigasi admin

--- resources/views/backend/layouts/navigation.blade.php
+++ resources/views/backend/layouts/navigation.blade.php
@@ -23,31 +23,31 @@
           <!-- NAVIGATION ADMIN -->
           @if (Auth::user()->role == 'admin')
           <li><a href="{{Help::url('dashboard')}}"><i class="fa fa-home"></i> Dashboard </a></li>
-          <li><a><i class="fa fa-archive"></i> Paket <span class="fa fa-chevron-down"></span></a>
+          <li><a><i class="fa fa-archive"></i> Packages <span class="fa fa-chevron-down"></span></a>
             <ul class="nav child_menu">
-              <li><a href="#">Tambah Paket</a></li>
-              <li><a href="#">Paket</a></li>
+              <li><a href="#">Add Package</a></li>
+              <li><a href="#">Packages</a></li>
             </ul>
           </li>
-          <li><a><i class="fa fa-list "></i> Menu <span class="fa fa-chevron-down"></span></a>
+          <li><a><i class="fa fa-list "></i> Menus <span class="fa fa-chevron-down"></span></a>
             <ul class="nav child_menu">
-              <li><a href="#">Tambah Menu</a></li>
-              <li><a href="#">Menu</a></li>
+              <li><a href="#">Add Menu</a></li>
+              <li><a href="#">Menus</a></li>
             </ul>
           </li>
-          <li><a><i class="fa fa-book"></i> Artikel <span class="fa fa-chevron-down"></span></a>
+          <li><a><i class="fa fa-book"></i> Articles <span class="fa fa-chevron-down"></span></a>
             <ul class="nav child_menu">
-              <li><a href="{{Help::url('article/create')}}">Tambah Artikel</a></li>
-              <li><a href="{{Help::url('article')}}">Artikel</a></li>
+              <li><a href="{{Help::url('article/create')}}">Add Article</a></li>
+              <li><a href="{{Help::url('article')}}">Articles</a></li>
             </ul>
           </li>
-          <li><a><i class="fa fa-envira"></i> Gallery <span class="fa fa-chevron-down"></span></a>
+          <li><a><i class="fa fa-envira"></i> Galleries <span class="fa fa-chevron-down"></span></a>
             <ul class="nav child_menu">
-              <li><a href="#">Tambah Foto</a></li>
-              <li><a href="#">Gallery</a></li>
+              <li><a href="#">Add photo</a></li>
+              <li><a href="#">Galleries</a></li>
             </ul>
           </li>
-          <li><a href="{{Help::url('/')}}"><i class="fa fa-comments"></i> Komentar </a></li>
+          <li><a href="{{Help::url('/')}}"><i class="fa fa-comments"></i> Comments </a></li>
           <li><a href="{{url('')}}"><i class="fa fa-user"></i> Profile </a></li>
           @endif
         </ul>
