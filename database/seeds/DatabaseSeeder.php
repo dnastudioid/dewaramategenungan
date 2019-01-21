58058eb77c4178f2043a1d83e51ff5b94f61e141
suhendra <agung.suhendra213@gmail.com>
2019-01-21 21:56:57 +0800

    gallery

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,10 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-<<<<<<< HEAD
         $this->call(MenusTableSeeder::class);
-=======
         $this->call(PackagesTableSeeder::class);
->>>>>>> c9e5967714d9e7e1d504369ddc80fdabeaa119b0
     }
 }
